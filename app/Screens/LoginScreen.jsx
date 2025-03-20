a8fb06680932abda6638b972157e64e5ab711357
Abdul100-han <abdulmuhd857@gmail.com>
2025-03-20 14:49:10 +0100

    implemenet the login screen ui

--- app/Screens/LoginScreen.jsx
+++ app/Screens/LoginScreen.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Image, Text, TouchableOpacity, View } from 'react-native'
 const LoginScreen = () => {
   return (
     <View>
-        <Text>login Scren content</Text>
+        <Image source={require('../../assets/images/market.jpg')}
+        className='w-full h-[400px] object-cover'
+        />
+
+        <View className='p-8 bg-white mt-[-20px] rounded-t-3xl'>
+            <Text className='text-[35px] font-bold'>Cmmunity Marketplace</Text>
+            
+            <Text className='text-[18px] text-slate-500 mt-6'>Buy sell MarketPlcae where you can sell and buy stuffs </Text>
+        </View>
+
+        <TouchableOpacity onPress={() => console.log('Sign In')} className='p-4 bg-blue-500 rounded-full mt-20'>
+            <Text className='text-white text-center text-[18px]'>Get Started</Text>
+        </TouchableOpacity>
     </View>
   )
 }
