26115b0209a9f271bc8f7d256589c27eb97572c1
Abdul100-han <abdulmuhd857@gmail.com>
2025-03-20 15:47:41 +0100

    implemented sign in and signout authentication with clerk

--- app/Screens/LoginScreen.jsx
+++ app/Screens/LoginScreen.jsx
@@ -1,6 +1,36 @@
 import React from 'react'
 import { Image, Text, TouchableOpacity, View } from 'react-native'
+import * as webBrowser from "expo-web-browser";
+import { useWarmUpBrowser } from '@/hooks/warmUpBrowser';
+import { useAuth } from '@clerk/clerk-expo';
+
+webBrowser.maybeCompleteAuthSession();
+
 const LoginScreen = () => {
+
+    useWarmUpBrowser();
+
+    const { startOAuthFlow } = useAuth({strategy: "oauth_google"})
+
+    const onPress = Reac.useCallback(async () => {
+        try {
+            const { createdSessionId, signIn, signUp, setActive } = 
+                await startOAuthFlow();
+
+                if (createdSessionId) {
+                    setActive({ session: createdSessionId });
+                } else {
+
+                }
+
+            
+        } catch (error) {
+            console.log("Outh Error", error);;
+            
+        }
+    })
+
+
   return (
     <View>
         <Image source={require('../../assets/images/market.jpg')}
@@ -13,7 +43,7 @@ const LoginScreen = () => {
             <Text className='text-[18px] text-slate-500 mt-6'>Buy sell MarketPlcae where you can sell and buy stuffs </Text>
         </View>
 
-        <TouchableOpacity onPress={() => console.log('Sign In')} className='p-4 bg-blue-500 rounded-full mt-20'>
+        <TouchableOpacity onPress={onPress} className='p-4 bg-blue-500 rounded-full mt-20'>
             <Text className='text-white text-center text-[18px]'>Get Started</Text>
         </TouchableOpacity>
     </View>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,13 +3,26 @@ import { SafeAreaView, Text, View } from 'react-native'
 
 import LoginScreen from './Screens/LoginScreen'
 
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
 import '../global.css'
 
 const _layout = () => {
   return (
+    <ClerkProvider publishableKey='pk_test_bGFzdGluZy1ob3JzZS00Ni5jbGVyay5hY2NvdW50cy5kZXYk'>
     <SafeAreaView>
+     
+
+      {/* auth  */}
+      <SignedIn>
+        <Text>You are signed in </Text>
+      </SignedIn>
+
+      <SignedOut>
       <LoginScreen />
+      </SignedOut>
+
     </SafeAreaView>
+    </ClerkProvider>
   )
 }
 
