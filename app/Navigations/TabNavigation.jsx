5c31228596d4d73cac4ee457aa92965d9ee40811
Abdul100-han <abdulmuhd857@gmail.com>
2025-04-12 19:24:25 +0100

    implemented react tab navigation

--- app/Navigations/TabNavigation.jsx
+++ app/Navigations/TabNavigation.jsx
@@ -4,15 +4,58 @@ import HomeScreen from '../Screens/HomeScreen';
 import ProfileScreen from '../Screens/ProfileScreen';
 import ExploreScreen from '../Screens/ExploreScreen';
 import AddPostScreen from '../Screens/AddPostScreen';
+import Entypo from '@expo/vector-icons/Entypo';
+import Feather from '@expo/vector-icons/Feather';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-const MyTabs = createBottomTabNavigator()
+
+const Tab = createBottomTabNavigator()
 const TabNavigation = () => {
   return (
-    <Tab.Navigator>
-        <Tab.Screen name='name' component={HomeScreen} />
-        <Tab.Screen name='explore' component={ExploreScreen} />
-        <Tab.Screen name='addpost' component={AddPostScreen} />
-        <Tab.Screen name='profile' component={ProfileScreen} />
+    <Tab.Navigator screenOptions={{
+      headerShown: false
+    }}>
+        <Tab.Screen name='home' component={HomeScreen} 
+        options={{
+          tabBarLabel:({color}) => {
+            <Text style={{color:color}}>Home</Text>
+          },
+          tabBarIcon:({color, size}) => {
+            <Entypo name="home" size={24} color={color} />
+          }
+        }}
+        />
+        <Tab.Screen name='explore' component={ExploreScreen}
+        options={{
+          tabBarLabel:({color}) => {
+            <Text style={{color:color}}>Explore</Text>
+          },
+          tabBarIcon:({color}) => {
+            <Feather name="search" size={24} color={color} />
+          }
+        }}
+        />
+        <Tab.Screen name='addpost' component={AddPostScreen}
+        options={{
+          tabBarLabel:({color}) => {
+            <Text style={{color:color}}>Add Post</Text>
+          },
+          tabBarIcon:({color, size}) => {
+            <AntDesign name="camerao" size={24} color={color} />
+          }
+        }}
+        />
+        <Tab.Screen name='profile' component={ProfileScreen} 
+        options={{
+          tabBarLabel:({color}) => {
+            <Text style={{color:color}}>Profile</Text>
+          },
+          tabBarIcon:({color, size}) => {
+            <Ionicons name="person-circle" size={24} color="black" />
+          }
+        }}
+        />
     </Tab.Navigator>
   )
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,16 +5,21 @@ import LoginScreen from './Screens/LoginScreen'
 
 import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
 import '../global.css'
+import { NavigationContainer } from '@react-navigation/native';
+import TabNavigation from './Navigations/TabNavigation'
+
 
 const _layout = () => {
   return (
     <ClerkProvider publishableKey='pk_test_bGFzdGluZy1ob3JzZS00Ni5jbGVyay5hY2NvdW50cy5kZXYk'>
     <SafeAreaView>
-     
+      
 
       {/* auth  */}
       <SignedIn>
-        <Text className='mt-10'>You are signed in </Text>
+        <NavigationContainer>
+          <TabNavigation />
+        </NavigationContainer>
       </SignedIn>
 
       <SignedOut>
